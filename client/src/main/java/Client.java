5e83f948c47e912c89541acce652a68eb2c747f7
luischarria11 <1108559191@u.icesi.edu.co>
2024-02-28 15:32:06 -0500

    Second Commit

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -6,20 +6,21 @@ import Demo.Response;
 public class Client {
 
     public static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         java.util.List<String> extraArgs = new java.util.ArrayList<>();
 
-        try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args,"config.client",extraArgs))
-        {
-            //com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -p 10000");
+        try (com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args, "config.client",
+                extraArgs)) {
+            // com.zeroc.Ice.ObjectPrx base =
+            // communicator.stringToProxy("SimplePrinter:default -p 10000");
             Response response = null;
             Demo.PrinterPrx service = Demo.PrinterPrx
                     .checkedCast(communicator.propertyToProxy("Printer.Proxy"));
 
             try {
-                
-                if(service == null) {
+
+                if (service == null) {
                     throw new Error("Invalid proxy");
                 }
 
@@ -28,22 +29,18 @@ public class Client {
                 String userInput;
 
                 do {
-                    System.out.println("Holaaaa");
                     userInput = sc.nextLine();
-                    
+
                     response = service.printString(username + ":" + hostname + " " + userInput);
 
-                    System.out.println("Respuesta del server: " + response.value + ", " + response.responseTime);
-                    
+                    System.out.println("Respuesta del server: " + response.value);
+
                 } while (!userInput.equalsIgnoreCase("exit"));
-             
-                
-            } catch(Exception ex){
+
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
 
-
-        
         }
     }
 }
